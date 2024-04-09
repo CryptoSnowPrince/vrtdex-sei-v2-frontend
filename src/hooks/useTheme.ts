1f6cd92bbc252d498426e9f9c4d6cbcb411310e4
simba6118 <LionKing.Simba6118@gmail.com>
2024-04-09 06:14:36 +0000

    update default theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -4,7 +4,7 @@ import { ThemeContext as StyledThemeContext } from 'styled-components'
 import { useTheme as useNextTheme } from 'next-themes'
 
 export const COOKIE_THEME_KEY = 'theme'
-export const THEME_DOMAIN = '.pancakeswap.finance'
+export const THEME_DOMAIN = '.v2.vrtdex.metabest.tech/'
 
 const useTheme = () => {
   const { resolvedTheme, setTheme } = useNextTheme()
@@ -12,14 +12,17 @@ const useTheme = () => {
 
   const handleSwitchTheme = (themeValue: 'light' | 'dark') => {
     try {
-      setTheme(themeValue)
-      Cookie.set(COOKIE_THEME_KEY, themeValue, { domain: THEME_DOMAIN })
+      setTheme('dark')
+      Cookie.set(COOKIE_THEME_KEY, 'dark', { domain: THEME_DOMAIN })
+      // setTheme(themeValue)
+      // Cookie.set(COOKIE_THEME_KEY, themeValue, { domain: THEME_DOMAIN })
     } catch (err) {
       // ignore set cookie error for perp theme
     }
   }
 
-  return { isDark: resolvedTheme === 'dark', theme, setTheme: handleSwitchTheme }
+  // return { isDark: resolvedTheme === 'dark', theme, setTheme: handleSwitchTheme }
+  return { isDark: true, theme, setTheme: handleSwitchTheme }
 }
 
 export default useTheme
