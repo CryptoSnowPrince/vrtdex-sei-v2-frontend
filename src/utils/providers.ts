54702018fa36bc66c1260c61b0b195e25098fbcf
CryptoSnowPrince <topdirector2017@gmail.com>
2024-09-13 08:37:49 +0000

    update rpc

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -1,7 +1,6 @@
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
-// export const BSC_PROD_NODE = process.env.NEXT_PUBLIC_NODE_PRODUCTION || 'https://evm-rpc.arctic-1.seinetwork.io'
-export const BSC_PROD_NODE = 'https://evm-rpc.arctic-1.seinetwork.io'
+export const BSC_PROD_NODE = 'https://evm-rpc.sei-apis.com'
 
 export const bscRpcProvider = new StaticJsonRpcProvider(BSC_PROD_NODE)
 
